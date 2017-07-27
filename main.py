48b3a080fd8946955bdf87c5358d60d41c37be5b
Nemecle <nemecle@protonmail.com>
2017-07-27 21:33:56 +0200

    moved img file into export folder.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-#!/bin/python
+#!/usr/bin/python
 """
 Gallery generation
 """
 
+import json
+
 
 def main():
+    """
+    generate gallery based on configuration file.
+
+    """
+
+    with open("structure.cfg") as structure:
+        data = json.load(structure)
+
+
 
     return
 
