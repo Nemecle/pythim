e54c68630e0acfd7a723ecfa211604b0b7c28731
Nemecle <nemecle@riseup.net>
2017-12-28 23:09:11 +0100

    reorganized project

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import os
 import jinja2
 import pprint
 
+from multiprocessing import Pool
+from PIL import Image
+
 
 
 #Jinja decoding issues
@@ -16,6 +19,39 @@ import sys
 # sys.setdefaultencoding('utf8')
 
 IMAGE_DIR = "img/"
+
+
+def gen_gal_old(dir):
+    """
+    DEPRECATED, WILL BE DELETED
+
+    """
+
+    galleries = {}
+                                                                         
+    model = open("gallery.html")
+    # template = jinja2.Template(model.read())
+                                                                         
+                                                                         
+    for f in files:
+        current_file = f.split("/")
+        for cdir in current_file[:-1]:
+            if cdir not in galleries:
+                galleries[cdir] = []
+                                                                         
+            galleries[cdir].append(f)
+                                                                         
+    # print str(galleries)
+                                                                         
+    for gallery in galleries:
+        files = galleries[gallery]
+        gallery_content = template.render(files=files)
+        file = open("export/" + str(gallery) + ".html","w")
+        file.write(gallery_content)
+        file.close()
+        print("created file " + str("export/") + str(gallery) + ".html")
+        return
+
 
 def get_directory_tree(path):
     """
@@ -45,36 +81,31 @@ def get_directory_tree(path):
     return file_list
 
 
-def gen_gal_old(dir):
+def thumbnail(image_details): 
+    """
+    https://stackoverflow.com/questions/8631076/what-is-the-fastest-way-to-generate-image-thumbnails-in-python
+
+    """
+
+    size, filename = image_details
+    try:
+        Image.open(filename).thumbnail(size).save("thumbnail_%s" % filename)
+        return 'OK'
+    except Exception as e: 
+        return e 
+
+
+def generate_thumbnails(img_dir, thumb_dir, size):
+    """
+    generate thumbnails for given directory in another directory
+    img_dir   -- directory of images to process
+    thumb_dir -- directory in which thumbnails are written
+    size      -- max size, in form of (x, y)
+
     """
 
 
-    """
-
-    galleries = {}
-                                                                         
-    model = open("gallery.html")
-    # template = jinja2.Template(model.read())
-                                                                         
-                                                                         
-    for f in files:
-        current_file = f.split("/")
-        for cdir in current_file[:-1]:
-            if cdir not in galleries:
-                galleries[cdir] = []
-                                                                         
-            galleries[cdir].append(f)
-                                                                         
-    # print str(galleries)
-                                                                         
-    for gallery in galleries:
-        files = galleries[gallery]
-        gallery_content = template.render(files=files)
-        file = open("export/" + str(gallery) + ".html","w")
-        file.write(gallery_content)
-        file.close()
-        print("created file " + str("export/") + str(gallery) + ".html")
-        return
+    return
 
 
 def main():
